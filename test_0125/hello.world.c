7555fcb0bd143a5a09497835a0f3c981a5a02175
ranyang_ry <3221592343@qq.com>
2026-02-07 13:20:42 +0800

    增加了一些代码

--- test_0125/hello.world.c
+++ test_0125/hello.world.c
@@ -1,5 +1,16 @@
+//#include <stdio.h>
+//int main() {
+//	printf("hello world!\n");
+//	return 0;
+//}
+
 #include <stdio.h>
-int main() {
-	printf("hello world!\n");
+int main()
+{
+	int i = 0;
+	for (i = 32; i <= 127; i++) {
+		printf("%c ", i);
+
+	}
 	return 0;
 }
